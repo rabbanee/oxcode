7e373de902060d669b9cbd5736c526b19a569c1e
rabbanee <daffarabbanee@gmail.com>
2020-09-27 08:31:56 +0700

    first commit

---
+++ app/Http/Controllers/AttractionController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Attraction;
+use Illuminate\Http\Request;
+
+class AttractionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Attraction  $attraction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Attraction $attraction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Attraction  $attraction
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Attraction $attraction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Attraction  $attraction
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Attraction $attraction)
+    {
+        //
+    }
+}
