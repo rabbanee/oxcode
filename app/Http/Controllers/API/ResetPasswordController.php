a60caa891888d7c67a5a1bdc72399a6f4e014b87
rabbanee <daffarabbanee@gmail.com>
2020-10-26 08:13:45 +0700

    change controller, notify, and table name to ResetPassword

---
+++ app/Http/Controllers/API/ResetPasswordController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\ResetPassword;
+use App\Models\User;
+use App\Notifications\ResetPasswordRequest;
+use App\Notifications\ResetPasswordSuccess;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class ResetPasswordController extends Controller
+{
+    /**
+     * Create token password reset
+     *
+     * @param  [string] email
+     * @return [string] message
+     */
+    public function create(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if (!$user)
+            return response()->error('Email is not found', 404);
+        $resetPassword = ResetPassword::updateOrCreate(
+            ['email' => $user->email],
+            [
+                'email' => $user->email,
+                'token' => \Str::random(60)
+            ]
+        );
+        if ($user && $resetPassword)
+            $user->notify(
+                new ResetPasswordRequest($resetPassword->token)
+            );
+        return response()->successWithMessage('Email has been sent for resetting your password');
+    }
+
+    /**
+     * Find token password reset
+     *
+     * @param  [string] $token
+     * @return [string] message
+     * @return [json] passwordReset object
+     */
+    public function find($token)
+    {
+        $resetPassword = ResetPassword::where('token', $token)
+            ->first();
+        if (!$resetPassword)
+            return response()->error('Reset password token is invalid!', 400);
+
+        if (Carbon::parse($resetPassword->updated_at)->addMinutes(720)->isPast()) {
+            $resetPassword->delete();
+            return response()->error('Reset password token is invalid!', 400);
+        }
+        return response()->successWithKey($resetPassword, 'reset_password_token');
+    }
+
+    /**
+     * Reset password
+     *
+     * @param  [string] email
+     * @param  [string] password
+     * @param  [string] password_confirmation
+     * @param  [string] token
+     * @return [string] message
+     * @return [json] user object
+     */
+    public function reset(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string|confirmed|min:8',
+            'token' => 'required|string'
+        ]);
+        $resetPassword = ResetPassword::where([
+            ['token', $request->token],
+            ['email', $request->email]
+        ])->first();
+        if (!$resetPassword)
+            return response()->error('Reset password token is invalid!', 400);
+        $user = User::where('email', $resetPassword->email)->first();
+        if (!$user)
+            return response()->error('Email is not found', 404);
+        $user->password = bcrypt($request->password);
+        $user->save();
+        $resetPassword->delete();
+        $user->notify(new ResetPasswordSuccess($resetPassword));
+        return response()->successWithKey($user, 'user');
+    }
+}
---
+++ app/Notifications/ResetPasswordSuccess.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordSuccess extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('Your password was reset.')
+            ->line('If you did change password, no further action is required.')
+            ->line('If you did not change password, protect your account.')
+            ->line('Please do not reply to this email with your password. We will never ask for your password, and we strongly discourage you from sharing it with anyone.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
